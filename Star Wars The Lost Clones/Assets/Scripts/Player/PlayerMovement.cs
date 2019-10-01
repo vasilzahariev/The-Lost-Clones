1008847f6001a58af8b65a520c196fda34d12fd5
Vasil Zahariev <vasilzahariev1707@abv.bg>
2019-10-01 09:36:01 +0300

    Adding Player movement

---
+++ Star Wars The Lost Clones/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Camera Camera;
+
+    public float Speed;
+
+    private float h;
+    private float v;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.h = Input.GetAxis("Horizontal") * this.Speed;
+        this.v = Input.GetAxis("Vertical") * this.Speed;
+    }
+
+    void FixedUpdate()
+    {
+        this.Rotate();
+
+        this.transform.Translate(this.h * Time.fixedDeltaTime, 0f, this.v * Time.fixedDeltaTime);
+    }
+
+    private void Rotate()
+    {
+        this.transform.rotation = new Quaternion(0f, this.Camera.transform.rotation.y * Time.fixedDeltaTime, 0f, this.Camera.transform.rotation.w);
+    }
+}
