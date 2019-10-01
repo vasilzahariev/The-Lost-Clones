3f2716fa10a9646e315b9d1d57fda91650bc355e
Vasil Zahariev <vasilzahariev1707@abv.bg>
2019-10-01 11:22:21 +0300

    Adding animations, better player rotation with the camera, simple jumping

--- Star Wars The Lost Clones/Assets/Scripts/Player/PlayerMovement.cs
+++ Star Wars The Lost Clones/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,30 +8,109 @@ public class PlayerMovement : MonoBehaviour
     public Camera Camera;
 
     public float Speed;
+    public float BackwardsSpeed;
+    public float RotationSpeed;
+    public float JumpForce;
+
+    private Animator animator;
+    private Rigidbody rg;
 
     private float h;
     private float v;
+    private bool forward;
+    private bool prevForward;
+    private bool jump;
 
     void Start()
     {
-        
+        this.animator = this.GetComponent<Animator>();
+        this.rg = this.GetComponent<Rigidbody>();
+
+        this.forward = false;
+        this.jump = false;
     }
 
     void Update()
     {
-        this.h = Input.GetAxis("Horizontal") * this.Speed;
-        this.v = Input.GetAxis("Vertical") * this.Speed;
+        if (Input.GetAxis("Vertical") < 0f)
+        {
+            this.h = Input.GetAxis("Horizontal") * this.BackwardsSpeed;
+            this.v = Input.GetAxis("Vertical") * this.BackwardsSpeed;
+
+            this.prevForward = this.forward;
+
+            this.forward = false;
+        }
+        else
+        {
+            this.h = Input.GetAxis("Horizontal") * this.Speed;
+            this.v = Input.GetAxis("Vertical") * this.Speed;
+
+            this.prevForward = this.forward;
+
+            this.forward = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            this.jump = true;
+        }
     }
 
     void FixedUpdate()
     {
         this.Rotate();
-
-        this.transform.Translate(this.h * Time.fixedDeltaTime, 0f, this.v * Time.fixedDeltaTime);
+        this.Move();
+        this.Jump();
     }
 
     private void Rotate()
     {
-        this.transform.rotation = new Quaternion(0f, this.Camera.transform.rotation.y * Time.fixedDeltaTime, 0f, this.Camera.transform.rotation.w);
+        Vector3 forward = this.Camera.transform.forward;
+        Vector3 right = this.Camera.transform.right;
+
+        Vector3 desiredMoveDirection = forward;
+
+        desiredMoveDirection.y = 0f;
+
+        Quaternion prevRotation = this.transform.rotation;
+
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), this.RotationSpeed);
+    }
+
+    private void Move()
+    {
+        this.transform.Translate(this.h * Time.fixedDeltaTime, 0f, this.v * Time.fixedDeltaTime);
+
+        if (this.v > 0f && this.forward && this.prevForward)
+        {
+            this.animator.SetFloat("Speed", 1f);
+        }
+        else if (this.v > 0f && !this.forward && this.prevForward)
+        {
+            this.animator.SetFloat("Speed", -1f);
+        }
+        else if (this.v < 0f && !this.forward && this.prevForward)
+        {
+            this.animator.SetFloat("Speed", -1f);
+        }
+        else if (this.v < 0f && this.forward && !this.prevForward)
+        {
+            this.animator.SetFloat("Speed", 1f);
+        }
+        else if (this.v == 0f && this.forward && this.forward)
+        {
+            this.animator.SetFloat("Speed", 0f);
+        }
+    }
+
+    private void Jump()
+    {
+        if (this.jump)
+        {
+            this.rg.AddForce(this.transform.up * this.JumpForce * this.rg.mass);
+
+            this.jump = false;
+        }
     }
 }
