72594c02b3adbc1cafd83d5055149fb803b86b0b
Vasil Zahariev <vasilzahariev1707@abv.bg>
2020-03-23 15:43:59 +0200

    Updating the animations and the movement. Implementing jump and slide, but they are both not yet completed.

---
+++ Game/Assets/Scripts/Player/PlayerEventHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEventHandler : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+
+    #region MonoMethods
+
+    void Start()
+    {
+        this.playerMovement = this.gameObject.GetComponent<PlayerMovement>();
+    }
+
+    #endregion
+
+    // TODO: Make it so when the player is sliding the capsule collider becomes smaller or rotates
+    #region Slide
+    public void StartSliding()
+    {
+        this.playerMovement.IsSliding = true;
+    }
+
+    public void EndSlide()
+    {
+        this.playerMovement.Slide = false;
+        this.playerMovement.IsSliding = false;
+    }
+
+    #endregion
+
+    #region Jump
+
+    public void StartJump()
+    {
+        this.playerMovement.MakeTheJump();
+    }
+
+    public void StartJumping()
+    {
+        this.playerMovement.Jumping = true;
+    }
+
+    #endregion
+}
--- Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    #region Properties
+
+    public Camera Camera;
+
+    public float RotationSpeed;
+
+    [HideInInspector]
+    public bool Slide;
+
+    [HideInInspector]
+    public bool IsSliding;
+
+    [HideInInspector]
+    public bool Jumping;
+
+    #endregion
+
+    #region Fields
+
     private Animator animator;
+    private Rigidbody rg;
 
     private float h;
     private float v;
@@ -14,23 +34,83 @@ public class PlayerMovement : MonoBehaviour
     private bool backwards;
     private bool left;
     private bool right;
+    private bool running;
+    private bool jump;
+
+    #endregion
+
+    #region MonoMethods
 
     void Start()
     {
         this.animator = this.gameObject.GetComponent<Animator>();
+        this.rg = this.gameObject.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         this.h = Input.GetAxis("Horizontal");
         this.v = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Run"))
+        {
+            this.running = true;
+        }
+        else if (Input.GetButtonUp("Run"))
+        {
+            this.running = false;
+        }
+        else if (Input.GetButton("Run"))
+        {
+            this.running = true;
+        }
+
+        if (Input.GetButtonDown("Slide") && !this.IsSliding && !this.Slide)
+        {
+            this.Slide = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            this.jump = true;
+        }
+
+        //Debug.Log("Jumping: " + this.Jumping + ". Jump: " + this.jump);
     }
 
     private void FixedUpdate()
     {
+        //if (Input.GetButton("Fire2"))
+        //{
+        //    this.Rotate();
+        //}
+
+        if (!this.IsSliding)
+        {
+            this.Rotate();
+        }
+
         this.Move();
 
         this.AnimationParser();
+    }
+
+    #endregion
+
+    // TODO: Add a little air mobility
+    #region Methods
+    private void Rotate()
+    {
+        Vector3 forward = this.Camera.transform.forward;
+        Vector3 right = this.Camera.transform.right;
+
+        Vector3 desiredMoveDirection = forward;
+
+        desiredMoveDirection.y = 0f;
+
+        Quaternion prevRotation = this.transform.rotation;
+
+        this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), this.RotationSpeed);
     }
 
     private void Move()
@@ -66,6 +146,31 @@ public class PlayerMovement : MonoBehaviour
             this.left = false;
             this.right = false;
         }
+
+        if (!this.forward && !this.backwards && !this.left && !this.right && this.running)
+        {
+            this.running = false;
+        }
+
+        if (!this.IsSliding)
+        {
+            if (!this.running)
+            {
+                this.Slide = false;
+            }
+            else
+            {
+                if (this.Slide && (this.backwards || this.left || this.right))
+                {
+                    this.Slide = false;
+                }
+            }
+        }
+    }
+
+    public void MakeTheJump()
+    {
+        this.rg.AddForce(this.transform.up * 500f);
     }
 
     private void AnimationParser()
@@ -74,5 +179,34 @@ public class PlayerMovement : MonoBehaviour
         this.animator.SetBool("Backwards", this.backwards);
         this.animator.SetBool("Left", this.left);
         this.animator.SetBool("Right", this.right);
+        this.animator.SetBool("IsRunning", this.running);
+        this.animator.SetBool("Slide", this.Slide);
+        this.animator.SetBool("IsSliding", this.Slide);
+        this.animator.SetBool("Jump", this.jump);
+        this.animator.SetBool("IsJumping", this.Jumping);
     }
+
+    #endregion
+
+    #region Collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Test");
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            this.jump = false;
+            this.Jumping = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && this.Jumping == true)
+        {
+            this.jump = false;
+            this.Jumping = false;
+        }
+    }
+    #endregion
 }
