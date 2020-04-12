3434acd99d863f1fdb309cc6db036b36c01f5e4f
Vasil Zahariev <vasilzahariev1707@abv.bg>
2020-04-12 11:50:00 +0300

    Adding little platforms

---
+++ Game/Assets/JumpPlatform.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPlatform : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(this.transform.up * 2500f);
+    }
+}
