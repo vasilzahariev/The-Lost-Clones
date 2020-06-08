8c7a821f7efbe5c6bdc528e16d74249c4468cd06
vasilzahariev <vasilzahariev1707@abv.bg>
2020-06-08 22:32:26 +0300

    Changed Platfor scripts variable names and added comments to variables, methods and classes

--- Game/Assets/Scripts/Others/MovePlatform.cs
+++ Game/Assets/Scripts/Others/MovePlatform.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovePlatform : MonoBehaviour
 {
-    public float Speed;
+    [SerializeField] private float _speed;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +15,6 @@ public class MovePlatform : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.gameObject.GetComponent<Rigidbody>().MovePosition((this.transform.position + new Vector3(0f, this.Speed, 0f) * Time.fixedDeltaTime));
+        this.gameObject.GetComponent<Rigidbody>().MovePosition((this.transform.position + new Vector3(0f, this._speed, 0f) * Time.fixedDeltaTime));
     }
 }
