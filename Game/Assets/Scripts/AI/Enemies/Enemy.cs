2351d35cc5367ecfb6bd60389a0f42e027e09944
vasilzahariev <vasilzahariev1707@abv.bg>
2020-06-08 22:26:55 +0300

    Changed Enemy AI scripts variable names and added comments to variables, methods and classes

--- Game/Assets/Scripts/AI/Enemies/Enemy.cs
+++ Game/Assets/Scripts/AI/Enemies/Enemy.cs
@@ -2,32 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// The base class of all enemies
+/// </summary>
+
 public class Enemy : MonoBehaviour, IDamagable<float>, IKillable, ITargetable, IShootable
 {
     #region Properties
 
     [Range(1f, 1000f)]
-    public float Health;
+    public float Health; // The health of the Enemy
 
-    [HideInInspector]
-    public GameObject Target;
+    public GameObject Target { get; protected set; } // The current target of the Enemy (where to look and shoot)
 
-    public Weapon Weapon;
+    public Weapon Weapon { get; protected set; } // The weapon of the Enemy
 
     #endregion
 
     #region Fields
 
-    protected Transform lookAt;
-    protected Transform eyes;
-    protected Transform shootAt;
+    protected Transform _lookAt; // Where other AI and the player should look at when targeting
+    protected Transform _eyes; // (To be improved) The eyes of the Enemy (the start point of the Raycast that looks for the player)
+    protected Transform _shootAt; // Where other AI and the player should shoot their bullets
 
-    protected Animator animator;
+    protected Animator _animator; // The Animator component of the Enemy object
 
     #endregion
 
     #region Methods
 
+    /// <summary>
+    /// This method controlls the animations
+    /// </summary>
     protected virtual void AnimationParser()
     {
     }
@@ -36,6 +43,11 @@ public class Enemy : MonoBehaviour, IDamagable<float>, IKillable, ITargetable, I
 
     #region InterfaceMethods
 
+    /// <summary>
+    /// This method is called when the Enemy has been hit by something that damages it
+    /// This method reduces the health of the Enemy by a specific values
+    /// </summary>
+    /// <param name="damage">The amount of health that is reduced</param>
     public virtual void TakeDamage(float damage)
     {
         float newHealth = this.Health - damage;
@@ -43,26 +55,45 @@ public class Enemy : MonoBehaviour, IDamagable<float>, IKillable, ITargetable, I
         this.Health = newHealth > 0 ? newHealth : 0f;
     }
 
+    /// <summary>
+    /// This method is called when the Enemy has reached 0 health and has to be destroyed
+    /// This method destroys the AI object (not script, but object)
+    /// </summary>
     public virtual void Die()
     {
         Destroy(this.gameObject);
     }
 
+    /// <summary>
+    /// This method is called to check if the Enemy is dead
+    /// </summary>
+    /// <returns>If the Enemy is dead</returns>
     public virtual bool isDead()
     {
         return this.Health <= 0f;
     }
 
+    /// <summary>
+    /// This method is called when you want to access the Look At
+    /// </summary>
+    /// <returns>Look At Transform</returns>
     public Transform GetLookAt()
     {
-        return this.lookAt;
+        return this._lookAt;
     }
 
+    /// <summary>
+    /// This method is called when you want to access the Shoot At
+    /// </summary>
+    /// <returns>Shoot At Transform</returns>
     public Transform GetShootAt()
     {
-        return this.shootAt;
+        return this._shootAt;
     }
 
+    /// <summary>
+    /// This method makes the Enemy look at and locks it at it's target
+    /// </summary>
     protected virtual void LockOnTarget()
     {
         this.transform.LookAt(this.Target.transform);
